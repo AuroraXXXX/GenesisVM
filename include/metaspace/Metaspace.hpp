d9d8223d58e6f3ea2aa18aec13c15da3bd37b8a8
xu kehu <xukh@fujitsu.com>
2025-03-12 10:48:05 +0800

    初步调整

--- include/metaspace/Metaspace.hpp
+++ include/metaspace/Metaspace.hpp
@@ -10,33 +10,6 @@
 class CharOStream;
 namespace metaspace {
     /**
-     * 定义Segment的级别所存储的类型
-     */
-    using SegmentLevel_t = int8_t;
-    enum class SegmentLevel: SegmentLevel_t {
-        LV_INVALID = -1,
-        LV_ROOT = 0,
-        LV_16M = LV_ROOT,
-        LV_8M,
-        LV_4M,
-        LV_2M,
-        LV_1M,
-        LV_512K,
-        LV_256K,
-        LV_128K,
-        LV_64K,
-        LV_32K,
-        LV_16K,
-        LV_8K,
-        LV_4K,
-        LV_2K,
-        LV_1K,
-        LV_NUM,//数量
-        LV_LOWEST = LV_16M,
-        LV_HIGHEST = LV_1K
-    };
-    #define SEGMENT_LV_FORMAT "lv%.02d"
-    /**
      * Region的字节数
      * 1个Volume是Region的整倍数
      */
---
+++ src/metaspace/SegmentLevel.cpp
@@ -0,0 +1,10 @@
+#include "metaspace/SegmentLevel.hpp"
+
+SegmentLevel::SegmentLevel_t SegmentLevel::get_optimal_level(size_t bytes){
+
+}
+
+size_t SegmentLevel::get_bytes(SegmentLevel_t level){
+
+    
+}
